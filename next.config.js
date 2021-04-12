a62f7225070b1b55527bf987592cf7f68710b5ad
ymizrahi <yitzhak.tech@gmail.com>
2021-04-13 00:04:00 +0300

    configuring cors for vercel

--- next.config.js
+++ next.config.js
@@ -2,12 +2,4 @@ module.exports = {
   images: {
     domains: ['www.google.co.uk'],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*',
-      },
-    ];
-  },
 };
--- pages/api/search.js
+++ pages/api/search.js
@@ -1,6 +1,30 @@
 import Response from '../../Response';
 import { API_KEY, CONTEXT_KEY, USE_DUMMY_DATA } from '../../keys';
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async (req, res) => {
