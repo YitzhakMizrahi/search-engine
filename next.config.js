7ff2c28f322136f793945e67d09c48648f2299b6
ymizrahi <yitzhak.tech@gmail.com>
2021-04-13 00:00:38 +0300

    configuring cors for vercel

--- next.config.js
+++ next.config.js
@@ -2,25 +2,11 @@ module.exports = {
   images: {
     domains: ['www.google.co.uk'],
   },
-  async headers() {
+  async rewrites() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
-        destination: 'https://ahttps://search-engine-khaki.vercel.app/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/api/:path*',
       },
     ];
   },
