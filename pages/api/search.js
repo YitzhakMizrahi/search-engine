08d04c2de6e6ba862258d5b27f0cbcf8efec684a
ymizrahi <yitzhak.tech@gmail.com>
2021-04-13 00:14:30 +0300

    configuring cors for vercel

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,43 +1,39 @@
 import Response from '../../Response';
 import { API_KEY, CONTEXT_KEY, USE_DUMMY_DATA } from '../../keys';
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
-
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default async (req, res) => {
-  const searchTerm = req.query;
-  const useDummyData = USE_DUMMY_DATA;
+const handler = nc()
+  // use connect based middleware
+  .use(cors())
+  .post(async (req, res) => {
+    const searchTerm = req.query;
+    const useDummyData = USE_DUMMY_DATA;
 
-  if (!searchTerm) return;
+    if (!searchTerm) return;
 
-  const data = useDummyData
-    ? Response
-    : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${searchTerm}`
-      ).then((response) => response.json());
+    const data = useDummyData
+      ? Response
+      : await fetch(
+          `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${searchTerm}`
+        ).then((response) => response.json());
 
-  res.status(200).json({ data: data });
-};
+    res.status(200).json({ data: data });
+  });
+
+// export default async (req, res) => {
+//   const searchTerm = req.query;
+//   const useDummyData = USE_DUMMY_DATA;
+
+//   if (!searchTerm) return;
+
+//   const data = useDummyData
+//     ? Response
+//     : await fetch(
+//         `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${searchTerm}`
+//       ).then((response) => response.json());
+
+//   res.status(200).json({ data: data });
+// };
+
+export default handler;
