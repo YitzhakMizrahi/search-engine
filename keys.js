5e678a9a81524dba3816979b71ece65118cc4f76
ymizrahi <yitzhak.tech@gmail.com>
2021-04-12 22:35:11 +0300

    adding autocomplete functionality

--- keys.js
+++ keys.js
@@ -3,3 +3,6 @@ export const API_KEY = process.env.API_KEY;
 
 // Context key - https://cse.google.com/cse/create/new
 export const CONTEXT_KEY = process.env.CONTEXT_KEY;
+
+// true for development & false for production
+export const USE_DUMMY_DATA = true
---
+++ pages/api/search.js
@@ -0,0 +1,19 @@
+import Response from '../../Response';
+import { API_KEY, CONTEXT_KEY, USE_DUMMY_DATA } from '../../keys';
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async (req, res) => {
+  const searchTerm = req.query;
+  const useDummyData = USE_DUMMY_DATA;
+
+  if (!searchTerm) return;
+
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${searchTerm}`
+      ).then((response) => response.json());
+
+  res.status(200).json({ data: data });
+};
