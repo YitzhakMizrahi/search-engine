391ed585cddbffb6412c5c4aefae8ec2b4d3e60d
ymizrahi <yitzhak.tech@gmail.com>
2021-04-15 20:11:13 +0300

    adding cursor-pointer to auto search results

--- components/AutoSearchResults.js
+++ components/AutoSearchResults.js
@@ -7,7 +7,7 @@ function AutoSearchResults({ results, search, input }) {
   const router = useRouter();
 
   return (
-    <div className="w-full rounded-3xl focus-within:shadow-lg rounded-t-none border border-gray-200 px-5 py-3 max-w-md shadow-lg sm:max-w-xl lg:max-w-2xl items-center">
+    <div className="w-full rounded-3xl focus-within:shadow-lg rounded-t-none border border-gray-200 px-5 py-3 max-w-md shadow-lg sm:max-w-xl lg:max-w-2xl items-center cursor-pointer">
       {results.data?.items?.map((result) => (
         <div key={result.link} className="mb-5 hover:bg-gray-50">
           <div className="group flex items-center">
