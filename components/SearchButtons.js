fede4d0d45d0f12e2df521b2c2c2ee74cccdfd8d
ymizrahi <yitzhak.tech@gmail.com>
2021-04-28 15:20:21 +0300

    fix: added onClick trigger to SearchButtons to work with enter key as well

--- components/SearchButtons.js
+++ components/SearchButtons.js
@@ -1,10 +1,12 @@
 function SearchButtons({ search, width }) {
   return (
-    <div className={`flex flex-col ${width} space-y-2 justify-center mt-8 mb-4 sm:space-y-0 sm:flex-row sm:space-x-4`}>
-      <button onMouseDown={search} className={`btn`}>
+    <div
+      className={`flex flex-col ${width} space-y-2 justify-center mt-8 mb-4 sm:space-y-0 sm:flex-row sm:space-x-4`}
+    >
+      <button onMouseDown={search} onClick={search} className={`btn`}>
         Google Search
       </button>
-      <button onMouseDown={search} className={`btn`}>
+      <button onMouseDown={search} onClick={search} className={`btn`}>
         I'm Feeling Lucky
       </button>
     </div>
