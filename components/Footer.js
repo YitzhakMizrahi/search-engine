b053953c90a01f724b634ca24baeef7267812580
ymizrahi <yitzhak.tech@gmail.com>
2021-05-28 17:21:10 +0300

    adding disclaimer to footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,13 @@ import { GlobeIcon } from '@heroicons/react/solid';
 function Footer() {
   return (
     <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
+      <p className="items-center px-8 py-3">
+        <span className="font-bold">Disclaimer: </span>
+        This is not the official Google SE. It is a redesign, built purely for
+        educational purpose.
+      </p>
       <div className="px-8 py-3 flex items-center fright">
-      <LocationMarkerIcon className="h-4 mr-1" /> 
+        <LocationMarkerIcon className="h-4 mr-1" />
         <p>Israel</p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-y-4 px-8 py-3 grid-flow-row-dense ">
@@ -14,14 +19,14 @@ function Footer() {
           2007
         </div>
         <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
-          <p>Advertising</p>
-          <p>Business</p>
-          <p>How Search works</p>
+          <p className="link">Advertising</p>
+          <p className="link">Business</p>
+          <p className="link">How Search works</p>
         </div>
         <div className="flex justify-center space-x-8 whitespace-nowrap md:ml-auto">
-          <p>Privacy</p>
-          <p>Terms</p>
-          <p>Settings</p>
+          <p className="link">Privacy</p>
+          <p className="link">Terms</p>
+          <p className="link">Settings</p>
         </div>
       </div>
     </footer>
